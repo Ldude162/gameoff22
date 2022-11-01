12f653ec38191e488e067bcbab2fe45365658ed0
TheZeldaBoi <63129353+Ldude162@users.noreply.github.com>
2022-11-01 15:43:33 -0700

    configured window for desktop game

--- src/desktop/src/com/thezeldaboi/gameoff22/DesktopLauncher.java
+++ src/desktop/src/com/thezeldaboi/gameoff22/DesktopLauncher.java
@@ -8,8 +8,10 @@ import com.thezeldaboi.gameoff22.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
 		config.setTitle("game-off-22");
+		config.setWindowedMode(800,480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
 		new Lwjgl3Application(new Game(), config);
 	}
 }
